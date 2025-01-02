c68ebc2d8844e6315ab25106dcafb63ee34b841b
ofirgady <OFirgady@gmail.com>
2025-01-02 15:17:28 +0200

    v9

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename='/miss-toy-proj/'>
     <Provider store={store}>
         <App />
     </Provider>
-    </BrowserRouter>
 )
