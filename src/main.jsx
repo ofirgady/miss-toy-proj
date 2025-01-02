ef6d0f3e58cae81003a5c36d15067e27ca879d1f
ofirgady <OFirgady@gmail.com>
2025-01-02 14:36:13 +0200

    update for github pages deployment v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './assets/style/scss/styles.scss'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
