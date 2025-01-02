26bbb48d550681298511c0fc7552b9476c8a096a
ofirgady <OFirgady@gmail.com>
2025-01-02 14:57:24 +0200

    b- deployment v7

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './assets/style/scss/styles.scss'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter basename='/miss-toy-proj/'>
     <Provider store={store}>
         <App />
     </Provider>
+    </BrowserRouter>
 )
